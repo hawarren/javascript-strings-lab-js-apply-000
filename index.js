1e1abf5348b88c1d7c0f1e44fccf9495ae39d2bb
Hanif Warren <hwarrendev@gmail.com>
2018-03-26 12:17:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson";
 
+<<<<<<< HEAD
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
+=======
+var greetSpecialGuest = "," + specialGuest + "!";
+>>>>>>> 8294afe11d7390c49d7d8eb8baa39c1df715f982
 
 var topic = "space";
 
